69c6e51f77a0b57a695c74f716466af489cf122c
Paul Nikitin <paulnikitin@gmail.com>
2016-03-11 00:14:55 +0300

    ADD: страничка /register.html с формой в стиле Twitter Bootstrap

--- src/Neox/UserBundle/Controller/DefaultController.php
+++ src/Neox/UserBundle/Controller/DefaultController.php
@@ -2,7 +2,13 @@
 
 namespace Neox\UserBundle\Controller;
 
+use Neox\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -29,6 +35,42 @@ class DefaultController extends Controller
     public function loginAction()
     {
         return $this->render('default/UserBundle/login.html.twig');
+    }
+
+    /**
+     * Страница регистрации
+     *
+     * @return string
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, array(
+                'invalid_message' => 'The password fields must match.',
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        return $this->render(
+            'default/UserBundle/register.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
     }
 
 }
