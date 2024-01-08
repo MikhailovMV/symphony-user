9de92100bdd391e6f6292889e2322770b831e037
Mikhail Mikhailov <mikhailoff@inbox.ru>
2024-01-08 10:41:36 +0300

    Create suball.c
    Add: Positive your Orange!

---
+++ web/css/suball.c
@@ -0,0 +1,11 @@
+@import('all.css');
+
+.block #element:html {
+  font-family: Courier;
+  font-style: stronger;
+  color: blue;
+  html: "God goodbay. Thwo men. Three trees. For grass. Five in bloom. Six. Spring. On.";
+  padding: 9px 10px 11px 12px;
+  title: "Iratial Face"
+}
+.
